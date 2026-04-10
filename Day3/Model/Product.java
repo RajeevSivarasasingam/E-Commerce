d676647532bd1bd8f094e9f245de7202d27ffa40
Rajeev <sivarajeev511@gmail.com>
2026-04-10 21:29:51 +0530

    Add Product class with attributes and methods

---
+++ Day3/Model/Product.java
@@ -0,0 +1,44 @@
+package vau.ac.lk.firstapp.model;
+
+public class Product  {
+    private   String name;
+    private  int quantity;
+    private   double price;
+
+    public  Product(String name,int quantity,double price)
+    {
+        this.name=name;
+        this.quantity=quantity;
+        this.price=price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public  double getPrice(){
+        return  price;
+    }
+    public String getName()
+    {
+        return  name;
+    }
+
+
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+
+    public  void  setName(String name)
+    {
+        this.name=name;
+    }
+
+    public  void  setPrice(double price)
+    {
+        this.price=price;
+    }
+
+
+
+
+}
