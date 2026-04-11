44c9996e1141f9fa577cd59844a92f5c5eee1a24
Rajeev <sivarajeev511@gmail.com>
2026-04-11 19:13:43 +0530

    Add Student class with age and name properties

---
+++ Day2/Student.java
@@ -0,0 +1,30 @@
+
+package vau.ac.lk.firstapp;
+
+public class Student  {
+
+    private int age;
+    private  String name ;
+
+    public  Student(String name,int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName()
+    {
+        return  name;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+
+
+}
